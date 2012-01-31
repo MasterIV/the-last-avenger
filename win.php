08241a458e8103a3a7b014159704e7d7339ec8e8
tobias.rojahn@gmail.com <tobias.rojahn@gmail.com@aa04efe1-8ddf-1e25-eea4-9e8a78f06e6c>
2012-01-31 15:26:38 +0000

    Initial Commit

---
+++ win.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head><title>The Last Avenger</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+		<div id="welcome" class="dialog">
+			<h3>The Last Avenger</h3>
+
+			<img src="img/end.png" alt="amagedon" style="display: block; margin: auto; border: 1px solid white;">
+
+			<p>Nachdem es der Menschheit gelungen ist genug Technologie für die Fertigstellung der Superwaffe zu erbeuten,
+			gelang es ihnen, den Kashiik einen empfindlichen Schlag zu versetzen. Die Städe auf der Heimatwelt der Kashiik
+			liegen in Trümmern. Wieder einmal hat die Menschheit gegenüber ihrem Wiedersacher behaupten können.</p>
+
+			<p>Langsam finden sich die Verstreuten überlebenden der Menschheit zusammen, um gemeinsam neue Planeten zu erschließen.
+			Die Menschheit hat noch einen langen und anstrengenden Weg vor sich, um zu alter Stärke zurückzukehren.
+			Ein uns bekannter Held wird dabei an ihrer Seite kämpfen,
+			bereit sich den Gefahren zu stellen, die in den Weiten des Universums lauern.</p>
+
+			<h2 align="center">The End</h2>
+		</div>
+
+<!-- <?php
+
+	if( isset($_GET['name'])) {
+		$fp = fopen( 'winner.txt', 'a' );
+		fputs( $fp, date( '[d.m.Y H:i] ').  htmlspecialchars( $_GET['name'] ) . "\n" );
+		fclose( $fp );
+	}
+
+?> -->
+
+  </body>
+</html>
